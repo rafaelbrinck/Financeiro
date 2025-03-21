59c2c583b3a41efdf844f9a46d55d8086c8e66ad
Rafael Brinckmann <rafabrinckmann@gmail.com>
2025-03-20 23:46:00 -0300

    Atualizando projeto

--- API/config/database.js
+++ API/config/database.js
@@ -1,10 +1,10 @@
 const { Client } = require("pg");
-require("dotenv").config();
+require("dotenv").config({ path: "./infos.env" });
 
 const conexao = {
   host: process.env.HOST,
-  port: process.env.PORT,
-  user: process.env.USERNAME,
+  port: Number(process.env.PORT),
+  user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
 };
